f67b1a9fa7b71627a2ac328f9bbddd1b9f510631
bela333 <b3kstudio@gmail.com>
2025-04-10 15:41:28 +0200

    fix: add exit code checks to build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,14 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 pandoc --top-level-division chapter --filter pandoc-minted.py -s --bibliography main.bib --citeproc -o build/main.tex .\main.md .\metadata.yaml
+if ($LASTEXITCODE -gt 0) {
+    throw "pandoc execution failed"
+}
 pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex
+if ($LASTEXITCODE -gt 0) {
+    throw "pdflatex first execution failed"
+}
 pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex
+if ($LASTEXITCODE -gt 0) {
+    throw "pdflatex second execution failed"
+}
