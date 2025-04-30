54b9054811f1fcb1b2ab12517d158c4e002ff8d4
bela333 <b3kstudio@gmail.com>
2025-04-30 16:10:51 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PANDOC_ACRONYMS_ACRONYMS=acronyms.json
+export PANDOC_ACRONYMS_ACRONYMS=acronyms.json
 ./pandoc-3.6.4/bin/pandoc --top-level-division chapter -f markdown+implicit_figures --filter pandoc-crossref --filter pandoc-minted.py --filter pandoc-acronyms -s --citeproc -o build/main.tex main.md metadata.yaml &&
- docker run --rm -v ./:/workdir danteev/texlive /bin/bash -c "pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex && pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex"
+ docker run --rm -v ./:/workdir danteev/texlive /bin/bash -c "pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex; pdflatex -shell-escape -interaction=batchmode -output-directory build build/main.tex"
 
